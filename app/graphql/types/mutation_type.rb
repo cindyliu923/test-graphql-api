10e2f07db62027b6e841f95869de974c41d30fbe
Cindy Liu <cindyliu923@gmail.com>
2021-08-03 16:36:41 +0800

    Add sign in user mutations

--- app/graphql/types/mutation_type.rb
+++ app/graphql/types/mutation_type.rb
@@ -2,5 +2,6 @@ module Types
   class MutationType < Types::BaseObject
     field :add_note, mutation: Mutations::AddNote
     field :create_user, mutation: Mutations::CreateUser
+    field :signin_user, mutation: Mutations::SignInUser
   end
 end
