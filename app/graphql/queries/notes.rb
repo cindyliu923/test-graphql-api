00f3c914fc7fbc48f137bf14257f078de4ce16e0
Cindy Liu <cindyliu923@gmail.com>
2021-08-04 09:53:10 +0800

    Add connection to notes which is changed from fetch notes

---
+++ app/graphql/queries/notes.rb
@@ -0,0 +1,10 @@
+module Queries
+  class Notes < Queries::BaseQuery
+
+    type Types::NoteType.connection_type, null: false
+
+    def resolve
+      Note.order(created_at: :desc)
+    end
+  end
+end
--- app/graphql/types/query_type.rb
+++ app/graphql/types/query_type.rb
@@ -7,7 +7,7 @@ module Types
     # Add root-level fields here.
     # They will be entry points for queries on your schema.
 
-    field :fetch_notes, resolver: Queries::FetchNotes
+    field :notes, resolver: Queries::Notes
     field :fetch_note, resolver: Queries::FetchNote
   end
 end
